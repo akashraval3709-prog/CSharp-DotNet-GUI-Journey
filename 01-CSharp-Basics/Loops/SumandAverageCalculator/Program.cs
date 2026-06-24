5e7f8313b8d434bbb03c606d08fabfd1e11c5b87
akash <akashraval3709@gmail.com>
2026-06-24 19:01:17 +0530

    feat: add sum and average calculator program
    Added documentation explaining the Sum and Average Calculator program, including concepts covered and learning outcomes.

---
+++ 01-CSharp-Basics/Loops/SumandAverageCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task02_SumAndAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,number ,sum=0,i=1;
+           Console.Write("How many numbers you want to add : ");
+            n = int.Parse( Console.ReadLine());
+            while( i <= n)
+            {
+                Console.Write("Enter a Number "+ i +" : ");
+                number =int.Parse(Console.ReadLine());
+                sum += number;
+                i++;
+
+            }
+            Console.WriteLine("Sum of the Number :" + sum);
+            double avg =(double) sum / n;
+            Console.WriteLine("Average of the Number :" + avg);
+
+        }
+    }
+}
