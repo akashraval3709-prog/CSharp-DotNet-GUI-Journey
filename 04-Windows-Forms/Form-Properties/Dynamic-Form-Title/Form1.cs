2820afe135c15662d0819e1a0f737a9d8dfe50e4
akash <akashraval3709@gmail.com>
2026-07-24 22:56:23 +0530

    Implement dynamic form title functionality
    Implemented form title updates using Form Load, TextChanged, and Button Click events to demonstrate runtime modification of the Form.Text property.

---
+++ 04-Windows-Forms/Form-Properties/Dynamic-Form-Title/Form1.cs
@@ -0,0 +1,16 @@
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.Text = txtBox.Text;
+          
+        }
+    }
+}
