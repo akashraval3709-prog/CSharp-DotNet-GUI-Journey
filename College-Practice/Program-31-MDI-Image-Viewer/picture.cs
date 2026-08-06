193b692f0c5d967e4ce4e59911cbb060bd286140
akash <akashraval3709@gmail.com>
2026-08-06 10:26:50 +0530

    Implement image zoom controls in MDI child form
    Implemented image viewing functionality.
    
    - Added Zoom In feature.
    - Added Zoom Out feature.
    - Added Reset Image functionality.
    - Preserved original image size for reset operations.

---
+++ College-Practice/Program-31-MDI-Image-Viewer/picture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DemoMDIForm
+{
+    public partial class picture : Form
+    {
+        private Size originalSize;
+        public picture()
+        {
+            InitializeComponent();
+        }
+
+
+        private void picture_Load(object sender, EventArgs e)
+        {
+            originalSize = pictureBox.Size;
+        }
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            pictureBox.Width = (int)(pictureBox.Width * 1.25);
+            pictureBox.Height = (int)(pictureBox.Height * 1.25);
+
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            pictureBox.Width = (int)(pictureBox.Width * 0.9);
+            pictureBox.Height = (int)(pictureBox.Height * 0.9);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pictureBox.Size = originalSize;
+        }
+    }
+}
