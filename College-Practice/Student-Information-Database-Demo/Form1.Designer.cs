f22e474c853e4f4382ac69736643ab95a6c6bf3a
akash <akashraval3709@gmail.com>
2026-08-30 20:41:58 +0530

    Design main student database management form
    Designed the main Windows Forms interface for student record management.
    
    - Added student input controls
    - Added Roll No, Name, Gender, and City fields
    - Added Male and Female RadioButtons
    - Added Add, Delete, Update, and Clear buttons
    - Added DataGridView for displaying student records
    - Added UI layout and control configuration

--- College-Practice/Student-Information-Database-Demo/Form1.Designer.cs
+++ College-Practice/Student-Information-Database-Demo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 namespace DBDemo
 {
-    partial class Form1
+    partial class From1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,11 +28,14 @@ namespace DBDemo
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(From1));
             panel1 = new Panel();
             label5 = new Label();
             dgvData = new DataGridView();
             panel2 = new Panel();
+            label7 = new Label();
+            dtBrithDate = new DateTimePicker();
+            label6 = new Label();
             btnClear = new Button();
             btnAdd = new Button();
             rdoFemale = new RadioButton();
@@ -44,7 +47,6 @@ namespace DBDemo
             label3 = new Label();
             label2 = new Label();
             label1 = new Label();
-            label6 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
             panel2.SuspendLayout();
@@ -54,9 +56,9 @@ namespace DBDemo
             // 
             panel1.Controls.Add(label5);
             panel1.Controls.Add(dgvData);
-            panel1.Location = new Point(407, 74);
+            panel1.Location = new Point(368, 74);
             panel1.Name = "panel1";
-            panel1.Size = new Size(813, 426);
+            panel1.Size = new Size(943, 426);
             panel1.TabIndex = 0;
             // 
             // label5
@@ -72,16 +74,19 @@ namespace DBDemo
             // 
             // dgvData
             // 
+            dgvData.AllowUserToAddRows = false;
             dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvData.Location = new Point(15, 36);
+            dgvData.Location = new Point(2, 45);
             dgvData.Name = "dgvData";
             dgvData.RowHeadersWidth = 51;
-            dgvData.Size = new Size(784, 308);
+            dgvData.Size = new Size(928, 336);
             dgvData.TabIndex = 9;
             dgvData.CellClick += dgvData_CellClick;
             // 
             // panel2
             // 
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(dtBrithDate);
             panel2.Controls.Add(label6);
             panel2.Controls.Add(btnClear);
             panel2.Controls.Add(btnAdd);
@@ -96,122 +101,27 @@ namespace DBDemo
             panel2.Controls.Add(label1);
             panel2.Location = new Point(12, 74);
             panel2.Name = "panel2";
-            panel2.Size = new Size(389, 426);
+            panel2.Size = new Size(350, 426);
             panel2.TabIndex = 0;
             // 
-            // btnClear
+            // label7
             // 
-            btnClear.BackColor = SystemColors.HotTrack;
-            btnClear.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnClear.ForeColor = Color.White;
-            btnClear.Location = new Point(218, 339);
-            btnClear.Name = "btnClear";
-            btnClear.Size = new Size(86, 42);
-            btnClear.TabIndex = 6;
-            btnClear.Text = "Cancel";
-            btnClear.UseVisualStyleBackColor = false;
-            btnClear.Click += btnClear_Click;
+            label7.AutoSize = true;
+            label7.Font = new Font("Cambria", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(30, 230);
+            label7.Name = "label7";
+            label7.Size = new Size(120, 20);
+            label7.TabIndex = 3;
+            label7.Text = "Date Of Birth :";
             // 
-            // btnAdd
+            // dtBrithDate
             // 
-            btnAdd.BackColor = SystemColors.HotTrack;
-            btnAdd.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnAdd.ForeColor = Color.White;
-            btnAdd.Location = new Point(117, 339);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(86, 42);
-            btnAdd.TabIndex = 5;
-            btnAdd.Text = "Add";
-            btnAdd.UseVisualStyleBackColor = false;
-            btnAdd.Click += btnAdd_Click;
-            // 
-            // rdoFemale
-            // 
-            rdoFemale.AutoSize = true;
-            rdoFemale.Font = new Font("Segoe UI", 9.6F, FontStyle.Bold);
-            rdoFemale.Location = new Point(234, 204);
-            rdoFemale.Name = "rdoFemale";
-            rdoFemale.Size = new Size(86, 25);
-            rdoFemale.TabIndex = 3;
-            rdoFemale.TabStop = true;
-            rdoFemale.Text = "Female";
-            rdoFemale.UseVisualStyleBackColor = true;
-            // 
-            // rdoMale
-            // 
-            rdoMale.AutoSize = true;
-            rdoMale.Font = new Font("Segoe UI", 9.6F, FontStyle.Bold);
-            rdoMale.Location = new Point(157, 205);
-            rdoMale.Name = "rdoMale";
-            rdoMale.Size = new Size(69, 25);
-            rdoMale.TabIndex = 2;
-            rdoMale.TabStop = true;
-            rdoMale.Text = "Male";
-            rdoMale.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
-            label4.Location = new Point(72, 205);
-            label4.Name = "label4";
-            label4.Size = new Size(79, 20);
-            label4.TabIndex = 2;
-            label4.Text = "Gender  :";
-            // 
-            // txtCity
-            // 
-            txtCity.BorderStyle = BorderStyle.FixedSingle;
-            txtCity.Location = new Point(151, 259);
-            txtCity.Name = "txtCity";
-            txtCity.Size = new Size(125, 27);
-            txtCity.TabIndex = 4;
-            // 
-            // txtRollNo
-            // 
-            txtRollNo.BorderStyle = BorderStyle.FixedSingle;
-            txtRollNo.Location = new Point(151, 87);
-            txtRollNo.Name = "txtRollNo";
-            txtRollNo.Size = new Size(125, 27);
-            txtRollNo.TabIndex = 0;
-            // 
-            // txtName
-            // 
-            txtName.BorderStyle = BorderStyle.FixedSingle;
-            txtName.Location = new Point(151, 146);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(125, 27);
-            txtName.TabIndex = 1;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
-            label3.Location = new Point(95, 259);
-            label3.Name = "label3";
-            label3.Size = new Size(48, 20);
-            label3.TabIndex = 3;
-            label3.Text = "City :";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
-            label2.Location = new Point(87, 150);
-            label2.Name = "label2";
-            label2.Size = new Size(63, 20);
-            label2.TabIndex = 1;
-            label2.Text = "Name :";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
-            label1.Location = new Point(74, 90);
-            label1.Name = "label1";
-            label1.Size = new Size(75, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Roll No :";
+            dtBrithDate.Format = DateTimePickerFormat.Short;
+            dtBrithDate.Location = new Point(153, 228);
+            dtBrithDate.Name = "dtBrithDate";
+            dtBrithDate.Size = new Size(125, 27);
+            dtBrithDate.TabIndex = 4;
+            dtBrithDate.Value = new DateTime(2026, 8, 30, 0, 0, 0, 0);
             // 
             // label6
             // 
@@ -224,16 +134,130 @@ namespace DBDemo
             label6.TabIndex = 7;
             label6.Text = "New Student Entry";
             // 
-            // Form1
+            // btnClear
+            // 
+            btnClear.BackColor = SystemColors.HotTrack;
+            btnClear.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClear.ForeColor = Color.White;
+            btnClear.Location = new Point(215, 349);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(86, 42);
+            btnClear.TabIndex = 8;
+            btnClear.Text = "Cancel";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = SystemColors.HotTrack;
+            btnAdd.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = Color.White;
+            btnAdd.Location = new Point(114, 349);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(86, 42);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // rdoFemale
+            // 
+            rdoFemale.AutoSize = true;
+            rdoFemale.Font = new Font("Segoe UI", 9.6F, FontStyle.Bold);
+            rdoFemale.Location = new Point(234, 188);
+            rdoFemale.Name = "rdoFemale";
+            rdoFemale.Size = new Size(86, 25);
+            rdoFemale.TabIndex = 3;
+            rdoFemale.TabStop = true;
+            rdoFemale.Text = "Female";
+            rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoMale
+            // 
+            rdoMale.AutoSize = true;
+            rdoMale.Font = new Font("Segoe UI", 9.6F, FontStyle.Bold);
+            rdoMale.Location = new Point(157, 189);
+            rdoMale.Name = "rdoMale";
+            rdoMale.Size = new Size(69, 25);
+            rdoMale.TabIndex = 2;
+            rdoMale.TabStop = true;
+            rdoMale.Text = "Male";
+            rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
+            label4.Location = new Point(72, 189);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Gender  :";
+            // 
+            // txtCity
+            // 
+            txtCity.BorderStyle = BorderStyle.FixedSingle;
+            txtCity.Location = new Point(149, 277);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(125, 27);
+            txtCity.TabIndex = 6;
+            // 
+            // txtRollNo
+            // 
+            txtRollNo.BorderStyle = BorderStyle.FixedSingle;
+            txtRollNo.Location = new Point(151, 71);
+            txtRollNo.Name = "txtRollNo";
+            txtRollNo.Size = new Size(125, 27);
+            txtRollNo.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            txtName.BorderStyle = BorderStyle.FixedSingle;
+            txtName.Location = new Point(151, 130);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
+            label3.Location = new Point(93, 277);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 20);
+            label3.TabIndex = 5;
+            label3.Text = "City :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(87, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Name :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 10.2F, FontStyle.Bold);
+            label1.Location = new Point(74, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Roll No :";
+            // 
+            // From1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1232, 629);
+            ClientSize = new Size(1335, 608);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
-            Name = "Form1";
+            Name = "From1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Student Registration & Records";
             Load += Form1_Load;
@@ -263,5 +287,7 @@ namespace DBDemo
         private Label label5;
         private Button btnClear;
         private Label label6;
+        private Label label7;
+        private DateTimePicker dtBrithDate;
     }
 }
